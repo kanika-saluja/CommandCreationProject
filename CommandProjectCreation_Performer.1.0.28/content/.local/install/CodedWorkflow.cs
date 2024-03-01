f3df06bc07c41f307f8a60ce3966416c695a7896
kanika <kanika.saluja@centricconsulting.com>
2024-03-01 06:59:11 -0500

    MapitAddress selection , projectType NET selection
    MapitAddress selection , projectType NET selection

---
+++ CommandProjectCreation_Performer.1.0.28/content/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace CommandProjectCreation_Performer
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
